e5b632576dae9e1d6bc77804c3fc10ac7bdf9c2b
quangtheHome <the.ngo@thinkvitals.com>
2025-03-09 08:53:34 +0700

    before using VITE

--- src/component/topBar/topBar.tsx
+++ src/component/topBar/topBar.tsx
@@ -6,15 +6,11 @@ const TopBar: React.FC = () => {
   return (
     <div className="top-bar">
       <Link to="https://tapmed.vn/tin-tuc/quay-la-trung-100-khach-hang-tham-gia-deu-se-rinh-duoc-giai-thuong-80.html">
-        {/* <Image
-          height="auto"
-          width="100%"
+        <img
           alt="July - Best price"
           src="https://tapmed.vn//upload_editor/posts/images/hihiihih%20(1).png"
           decoding="async"
-          className="top-banner"
-          style={{}}
-        /> */}
+        />
       </Link>
     </div>
   );

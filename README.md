0123ebecaf372579800e859f990bf73f08348a5f
quangTheHome <the.ngo@thinkvitals.com>
2025-04-12 15:09:17 +0700

    update

--- README.md
+++ README.md
@@ -1,15 +1,15 @@
 # React + TypeScript + Vite
 1. Cài đặt PM2
-npm install -g pm2
+sudo npm install -g pm2
 
 2. Build dự án
-npm run build
+sudo npm run build
 
 3. Cài đặt một server tĩnh (serve)
-npm install -g serve
+sudo npm install -g serve
 
 4. Chạy dự án với PM2
-pm2 start serve --name "tapmed-project" -- -s dist -l 3001
+sudo pm2 start serve --name "tapmed-project" -- -s dist -l 3001
 
 
 
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,4 @@
 import "./App.css";
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
 import SectionBanner from "./component/sectionBanner/sectionBanner";
 import SectionHotSale from "./component/sectionHotSale/sectionHotSale";
 import SectionProductNew from "./component/sectionProductnew/sectionProductNew";
@@ -25,63 +22,6 @@ function App() {
       <SectionTestimonials />
       <SectionPartner />
     </>
-=======
->>>>>>> 4c18ce9180df758e50ebb906bb1adf97ec0507ca
-import { paths } from "./utils/contanst";
-import TopBar from "./component/topBar/topBar";
-// import "../src/assets/css/bootstrap.min.css";
-// import "../src/assets/css/blogs.css";
-// import "../src/assets/css/breadcrumb.css";
-// import "../src/assets/css/cart.css";
-// import "../src/assets/css/collections.css";
-// import "../src/assets/css/dashboard.css";
-// import "../src/assets/css/order.css";
-// import "../src/assets/css/pages.css";
-// import "../src/assets/css/product.css";
-// import "../src/assets/css/responsive.css";
-// import "../src/assets/css/styles.css";
-// import "../src/assets/css/swiper.css";
-function App() {
-  return (
-    <div className="app-container">
-      <h1>App</h1>
-      {/* <nav>
-        <Link to={paths.home}>Home</Link>
-        <Link to={paths.policy}>Policy</Link>
-        <Link to={paths.product}>Product</Link>
-        <Link to={paths.promotion}>Promotion</Link>
-        <Link to={paths.quickOrder}>Quick Order</Link>
-      </nav> */}
-    </div>
-<<<<<<< HEAD
-=======
-import SectionBanner from "./component/sectionBanner/sectionBanner";
-import SectionHotSale from "./component/sectionHotSale/sectionHotSale";
-import SectionProductNew from "./component/sectionProductnew/sectionProductNew";
-import SectionAbout from "./component/sectionAbout/sectionAbout";
-import SectionSteps from "./component/sectionSteps/sectionSteps";
-import SectionService from "./component/sectionService/sectionService";
-import SectionBlog from "./component/sectionBlog/sectionBlog";
-import SectionTestimonials from "./component/sectionTestimonitals/sectionTestimonials";
-import SectionPartner from "./component/sectionPartner/sectionPartner";
-
-function App() {
-  return (
-    <>
-      <SectionBanner />
-      <SectionHotSale />
-      <SectionProductNew />
-      <SectionAbout />
-      <SectionSteps />
-      <SectionService />
-      <SectionBlog />
-      <SectionTestimonials />
-      <SectionPartner />
-    </>
->>>>>>> dev4_cms
-=======
->>>>>>> main
->>>>>>> 4c18ce9180df758e50ebb906bb1adf97ec0507ca
   );
 }
 
